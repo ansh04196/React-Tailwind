ce34c7bd240a752aa66e9295479e511eec4e87ff
Ansh Chouksey <anshchouksey08@gmail.com>
2023-11-21 01:44:22 +0530

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div> 
+      <img src="https://images.pexels.com/photos/19017576/pexels-photo-19017576/free-photo-of-sunflowers-photograpy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      <h1 className='text-2xl bg-green-500 p-3 rounded'>A card </h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non adipisci nam officia perferendis voluptatibus saepe quos optio in, dolores fuga!</p>
+    </div>
+  )
+}
+
+export default Card
